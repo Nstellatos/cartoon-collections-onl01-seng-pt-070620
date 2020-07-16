1ecd38fb80325423c33ca2005d4d530929a26999
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-16 03:05:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)
   array.collect do |calls|
     planeteer_calls << "#{calls.capitalize}!"
   end
-
+end
 def long_planeteer_calls# code an argument here
   # Your code here
 end
